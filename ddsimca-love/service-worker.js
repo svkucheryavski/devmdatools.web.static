b943f36a5479f8e97d1e0a47eb61e484495fcd53
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-04-11 15:20:06 +0200

    ddsimca v. 2.1.7 + ddsimca-love v. 1.0.2 - small improvements and bug fixes

--- ddsimca-love/service-worker.js
+++ ddsimca-love/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '1.0.1a';
+const CACHE_VERSION = '1.0.2a';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(`${CACHE_NAME}-${CACHE_VERSION}`).then((cache) => {
