ca82b42d1c9fe70f9131ddec4911381dbd0b8244
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-06-08 13:52:26 +0200

    ddsimca-love: v.1.1.0 - added rigorous mode and prepared for oficial release

--- ddsimca-love/service-worker.js
+++ ddsimca-love/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '1.0.6a';
+const CACHE_VERSION = '1.1.0a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
