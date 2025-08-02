ef13536631cda5b830ddee426185ca24aec43595
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-08-02 17:08:12 +0200

    pls: v. 1.2.5 - fixed a small bug

--- pls/service-worker.js
+++ pls/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'pls-cache';
-const CACHE_VERSION = '1.2.4a';
+const CACHE_VERSION = '1.2.5a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
