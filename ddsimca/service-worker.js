c59be1048e6ae340c2dd66be007e2f63e6c767ea
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-09-14 13:34:15 +0200

    sddsimca: v. 2.2.7

--- ddsimca/service-worker.js
+++ ddsimca/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'ddsimca-cache';
-const CACHE_VERSION = '2.2.6b';
+const CACHE_VERSION = '2.2.7a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
