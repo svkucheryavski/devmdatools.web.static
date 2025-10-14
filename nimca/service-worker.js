4a8e167a1ddb27f333f0a4baef6e4536aeef7487
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2025-10-14 14:11:41 +0200

    nimca: v. 1.1.0

--- nimca/service-worker.js
+++ nimca/service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 const CACHE_NAME = 'nimca-cache';
-const CACHE_VERSION = '1.0.1a';
+const CACHE_VERSION = '1.1.0a';
 self.addEventListener('install', (event) => {
    self.skipWaiting();
    event.waitUntil(
